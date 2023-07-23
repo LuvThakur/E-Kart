fa512b877148abbdae14123701271bbd37bb775c
LuvThakur <luvjadaun786@gmail.com>
2023-07-23 14:14:47 +0530

    Ekart-app

---
+++ README.md
@@ -0,0 +1,5 @@
+# Getting Started with Create 
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+![Screenshot (220)](https://github.com/lokeshhhh25/e-kart-celebal/assets/110277723/6391c0c6-24ab-4e4e-9fab-42b1b2ac0eec)
---
+++ src/constData.js
@@ -0,0 +1,257 @@
+export const Data = [
+    {
+        "_id": "1",
+        "title": "Nike Shoes 01",
+        "src": "https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        "description": "Lightweight and breathable footwear designed to enhance speed and agility for athletes.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 23,
+        "colors": [
+            "red",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "2",
+        "title": "Nike Shoes 02",
+        "src": "https://images.unsplash.com/photo-1622920883841-5bf72e392d8f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=694&q=80",
+        "description": " Fashion-forward sneakers combining style and comfort for city dwellers on the go.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 19,
+        "colors": [
+            "red",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "3",
+        "title": "Nike Shoes 03",
+        "src": "https://images.unsplash.com/photo-1579446565308-427218a2c60e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=725&q=80",
+        "description": " Durable and rugged boots built to conquer challenging terrains and provide exceptional traction.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 50,
+        "colors": [
+            "lightblue",
+            "white",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "4",
+        "title": "Nike Shoes 04",
+        "src": "https://images.unsplash.com/photo-1679284392467-347236c31c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        "description": " Versatile athletic footwear that offers flexibility and support for various exercise routines.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 15,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "5",
+        "title": "Nike Shoes 05",
+        "src": "https://images.unsplash.com/photo-1595341888016-a392ef81b7de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=879&q=80",
+        "description": "Classic design meets modern comfort, perfect for adding a vintage touch to any outfit.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 10,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "6",
+        "title": "Nike Shoes 06",
+        "src": "https://images.unsplash.com/photo-1549298916-f52d724204b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=813&q=80",
+        "description": "Cushioned and supportive shoes engineered to promote a comfortable walking experience and reduce fatigue.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 17,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "7",
+        "title": "Nike Shoes 04",
+        "src": "https://images.unsplash.com/photo-1679284392467-347236c31c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        "description": " Versatile athletic footwear that offers flexibility and support for various exercise routines.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 15,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "8",
+        "title": "Nike Shoes 05",
+        "src": "https://images.unsplash.com/photo-1595341888016-a392ef81b7de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=879&q=80",
+        "description": "Classic design meets modern comfort, perfect for adding a vintage touch to any outfit.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 10,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "9",
+        "title": "Nike Shoes 04",
+        "src": "https://images.unsplash.com/photo-1679284392467-347236c31c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        "description": " Versatile athletic footwear that offers flexibility and support for various exercise routines.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 15,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "10",
+        "title": "Nike Shoes 05",
+        "src": "https://images.unsplash.com/photo-1595341888016-a392ef81b7de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=879&q=80",
+        "description": "Classic design meets modern comfort, perfect for adding a vintage touch to any outfit.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 10,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "11",
+        "title": "Nike Shoes 06",
+        "src": "https://images.unsplash.com/photo-1549298916-f52d724204b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=813&q=80",
+        "description": "Cushioned and supportive shoes engineered to promote a comfortable walking experience and reduce fatigue.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 17,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "12",
+        "title": "Nike Shoes 06",
+        "src": "https://images.unsplash.com/photo-1549298916-f52d724204b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=813&q=80",
+        "description": "Cushioned and supportive shoes engineered to promote a comfortable walking experience and reduce fatigue.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 17,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "13",
+        "title": "Nike Shoes 05",
+        "src": "https://images.unsplash.com/photo-1595341888016-a392ef81b7de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=879&q=80",
+        "description": "Classic design meets modern comfort, perfect for adding a vintage touch to any outfit.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 10,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "14",
+        "title": "Nike Shoes 04",
+        "src": "https://images.unsplash.com/photo-1679284392467-347236c31c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        "description": " Versatile athletic footwear that offers flexibility and support for various exercise routines.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 15,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "15",
+        "title": "Nike Shoes 05",
+        "src": "https://images.unsplash.com/photo-1595341888016-a392ef81b7de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=879&q=80",
+        "description": "Classic design meets modern comfort, perfect for adding a vintage touch to any outfit.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 10,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "16",
+        "title": "Nike Shoes 06",
+        "src": "https://images.unsplash.com/photo-1549298916-f52d724204b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=813&q=80",
+        "description": "Cushioned and supportive shoes engineered to promote a comfortable walking experience and reduce fatigue.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 17,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    },
+    {
+        "_id": "17",
+        "title": "Nike Shoes 06",
+        "src": "https://images.unsplash.com/photo-1549298916-f52d724204b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=813&q=80",
+        "description": "Cushioned and supportive shoes engineered to promote a comfortable walking experience and reduce fatigue.",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 17,
+        "colors": [
+            "orange",
+            "black",
+            "crimson",
+            "teal"
+        ],
+        "count": 1
+    }
+
+]
